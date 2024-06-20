7506d97469cae225754239c3097b20435106d8e7
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-20 22:56:44 +0530

    remove material-tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Button } from "@material-tailwind/react";
-
 export default function App() {
-  return <Button color="blue">Hello World</Button>;
+  return <h1 className="text-red-400">Hello, world!</h1>;
 }
+
