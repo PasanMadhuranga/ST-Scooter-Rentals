44393fd454e8cc60030a78e09ee0305a4fb4d2d6
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-21 14:24:25 +0530

    basic header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import { Box } from '@mui/material'
 
 export default function Footer() {
   return (
-    <Box height={64} sx={{ bgcolor: 'primary.main' }}>
-        Header
+    <Box height={64} >
+        Footer
     </Box>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,8 @@
-import { Box } from '@mui/material'
-
+import { Box } from "@mui/material";
 export default function Header() {
   return (
-    <Box height={64} sx={{ bgcolor: 'primary.main' }}>
-        Header
+    <Box height={64} >
+      Header
     </Box>
-  )
+  );
 }
