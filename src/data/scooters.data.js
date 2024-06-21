edebdaf6bf006fcefaba0050a23849fd2c540d41
SARATHCHANDRA D.M.P.M <107733419+PasanMadhuranga@users.noreply.github.com>
2024-06-21 13:03:54 +0530

    create scooter.jsx file

---
+++ src/data/scooters.data.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 1,
+    name: "Scooter 1",
+    description: "Scooter 1 description",
+    price: 10,
+    imageUrls: [
+      "https://motoroctane.com/wp-content/uploads/2016/05/New-Honda-Dio-2017-Yellow.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW9LZOIJ63aprzi4SZkkY557bvFG7FYgi98A&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlwTA6OSz6p4-rfFO7AMFKFVkYH0lq5g_fOA&s",
+    ],
+  },
+  {
+    id: 2,
+    name: "Scooter 2",
+    description: "Scooter 2 description",
+    price: 20,
+    imageUrls: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlwTA6OSz6p4-rfFO7AMFKFVkYH0lq5g_fOA&s",
+      "https://motoroctane.com/wp-content/uploads/2016/05/New-Honda-Dio-2017-Yellow.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW9LZOIJ63aprzi4SZkkY557bvFG7FYgi98A&s",
+    ],
+  },
+  {
+    id: 3,
+    name: "Scooter 3",
+    description: "Scooter 3 description",
+    price: 30,
+    imageUrls: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW9LZOIJ63aprzi4SZkkY557bvFG7FYgi98A&s",
+      "https://motoroctane.com/wp-content/uploads/2016/05/New-Honda-Dio-2017-Yellow.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlwTA6OSz6p4-rfFO7AMFKFVkYH0lq5g_fOA&s",
+    ],
+  },
+];
